4cf214110b014bad045e8b2af47ff6389e5169d3
Cuppuchino228 <okakentok540@gmail.com>
2026-03-26 13:11:13 +0700

    frontend-project-44

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
-function app () {
+import getName from "../scr/cli.js"
+
+export default function app () {
     console.log("Welcome to the Brain Games!")
+
+    getName()
 }
 
 app()
